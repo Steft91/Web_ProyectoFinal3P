04a810a97c68dcecb08fed6034ab719bae8c7a42
Steft91 <stefany152005@gmail.com>
2025-02-24 14:52:16 -0500

    First commit

---
+++ MVCAlquilerVehiculos/AppMVCAlquilerVehiculos/CapaDatos/TipoMedicamentoDAL.cs
@@ -0,0 +1,156 @@
+﻿using CapaEntidad;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace CapaDatos
+{
+    public class TipoMedicamentoDAL : CadenaDAL
+    {
+        public TipoMedicamentoCLS recuperarTipoMedicamento(int idTipoMedicamento)
+        {
+            TipoMedicamentoCLS oTipoMedicamentoCLS = null;
+
+            using (SqlConnection cn = new SqlConnection(cadena))
+            {
+                try
+                {
+                    cn.Open();
+                    using (SqlCommand cmd = new SqlCommand("select IDTIPOMEDICAMENTO, NOMBRE, DESCRPCION\"\r\n+ \"from TipoMedicamento where BHABILITADO = 1 and IDTIPOMEDICAMENTO = @idTipoMedicamento", cn))
+                    {
+                        cmd.CommandType = CommandType.Text;
+                        cmd.Parameters.AddWithValue("@idTipoMedicamento", idTipoMedicamento);
+
+                        using (SqlDataReader dr = cmd.ExecuteReader())
+                            if (dr != null) {
+                                while (dr.Read())
+                                {
+                                    oTipoMedicamentoCLS = new TipoMedicamentoCLS();
+                                    oTipoMedicamentoCLS.idTipoMedicamento = dr.IsDBNull(0) ? 0 : dr.GetInt32(0);
+                                    oTipoMedicamentoCLS.nombre = dr.IsDBNull(1) ? string.Empty : dr.GetString(1);
+                                    oTipoMedicamentoCLS.descripcion = dr.IsDBNull(2) ? "" : dr.GetString(2);
+                                }
+                                    cn.Close();
+                                }
+                            }                           
+                        }
+                catch (Exception)
+                {
+                    cn.Close();
+                    oTipoMedicamentoCLS = null;
+                    throw;
+                }
+            }
+            return oTipoMedicamentoCLS;
+        }
+
+
+        public int GuardarTipoMedicamento(TipoMedicamentoCLS otipoMedicamentoCLS)
+        {
+            int rpta = 0;
+            using (SqlConnection cn = new SqlConnection(cadena))
+                try
+                {
+                    cn.Open();
+
+                    using (SqlCommand cmd = new SqlCommand("insert into TipoMedicamento(NOMBRE,DESCRIPCION, BHABILITADO)" +
+                        "values(@nombre, @descripcion, 1)", cn))
+                    {
+                        cmd.CommandType = CommandType.Text;
+                        cmd.Parameters.AddWithValue("@nombre", otipoMedicamentoCLS.nombre);
+                        cmd.Parameters.AddWithValue("@descripcion", otipoMedicamentoCLS.descripcion);
+
+                        rpta = cmd.ExecuteNonQuery();
+                    }
+                } catch (Exception ex)
+                {
+                    Console.WriteLine("ex ocurrida en dal" + ex);
+                    cn.Close();
+                }
+                return rpta;
+        }
+        public List<TipoMedicamentoCLS> listarTipoMedicamento()
+        {
+            List<TipoMedicamentoCLS> lista = new List<TipoMedicamentoCLS>();
+
+            using (SqlConnection cn = new SqlConnection(cadena)) 
+            {
+                try
+                {
+                    cn.Open();
+                    using (SqlCommand cmd = new SqlCommand("uspListarTipoMedicamento", cn))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+
+                        using (SqlDataReader dr = cmd.ExecuteReader())
+                        {
+                            while (dr.Read())
+                            {
+                                TipoMedicamentoCLS oTipoMedicamentoCLS = new TipoMedicamentoCLS
+                                {
+                                    idTipoMedicamento = dr.IsDBNull(0) ? 0 : dr.GetInt32(0),
+                                    nombre = dr.IsDBNull(1) ? "" : dr.GetString(1),
+                                    descripcion = dr.IsDBNull(2) ? "" : dr.GetString(2)
+                                };
+
+                                lista.Add(oTipoMedicamentoCLS);
+                            }
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    lista = null;
+                    throw;
+                }
+            }
+            return lista;
+        }
+
+        public List<TipoMedicamentoCLS> filtrarTipoMedicamento(string nombre)
+        {
+            List<TipoMedicamentoCLS> lista = new List<TipoMedicamentoCLS>();
+
+            using (SqlConnection cn = new SqlConnection(cadena))
+            {
+                try
+                {
+                    cn.Open();
+                    using (SqlCommand cmd = new SqlCommand("uspFiltrarTipoMedicamento", cn))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@nombretipomedicamento", nombre ?? "");
+
+                        using (SqlDataReader dr = cmd.ExecuteReader())
+                        {
+                            while (dr.Read())
+                            {
+                                TipoMedicamentoCLS oTipoMedicamentoCLS = new TipoMedicamentoCLS
+                                {
+                                    idTipoMedicamento = dr.IsDBNull(0) ? 0 : dr.GetInt32(0),
+                                    nombre = dr.IsDBNull(1) ? "" : dr.GetString(1),
+                                    descripcion = dr.IsDBNull(2) ? "" : dr.GetString(2)
+                                };
+
+                                lista.Add(oTipoMedicamentoCLS);
+                            }
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    lista = null;
+                    throw;
+                }
+            }
+            return lista;
+        }
+    }
+}
+
+
+//select IDTIPOMEDICAMENTO, NOMBRE, DESCRPCION"
+//+ "from TipoMedicamento where BHABILITADO = 1 and IDTIPOMEDICAMENTO = @idtipomedicamento", cn)
---
+++ MVCAlquilerVehiculos/AppMVCAlquilerVehiculos/MiPrimeraAppMVC/Controllers/TipoMedicamentoController.cs
@@ -0,0 +1,56 @@
+﻿using CapaDatos;
+using CapaEntidad;
+using CapaNegocios;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MiPrimeraAppMVC.Controllers
+{
+    public class TipoMedicamentoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Inicio() {
+            return View();
+        }
+        public IActionResult SinMenu() {
+            return View();
+        }
+
+        public List<TipoMedicamentoCLS> listarTipoMedicamento()
+        {
+            TipoMedicamentoDAL obj = new TipoMedicamentoDAL();
+            return obj.listarTipoMedicamento();
+        }
+
+        public List<TipoMedicamentoCLS> filtrarTipoMedicamento(string nombre)
+        {
+            TipoMedicamentoDAL obj = new TipoMedicamentoDAL();
+            return obj.filtrarTipoMedicamento(nombre);
+        }
+
+        public int GuardarTipoMedicamento(TipoMedicamentoCLS otipoMedicamentoCLS)
+        {
+            TipoMedicamentoBL obj = new TipoMedicamentoBL();
+            var formValues = Request.Form;
+
+
+            foreach (var key in formValues.Keys)
+            {
+                var value = formValues[key];
+                Console.WriteLine($"{key}: {value}");
+            }
+            return obj.GuardarTipoMedicamento(otipoMedicamentoCLS);
+
+        }
+
+        public TipoMedicamentoCLS recuperarTipoMedicamento(int idTipoMedicamento)
+        {
+            TipoMedicamentoBL obj = new TipoMedicamentoBL();
+            return obj.recuperarTipoMedicamento(idTipoMedicamento);
+
+        }
+
+    }
+}
